14c10df0be87dfbaacd129c28ba7400dc6c7a5e3
J LEELA VARDHAN <leelavardhanjanigarla7981@gmail.com>
2026-03-03 10:35:05 +0530

    Add files via upload

---
+++ 03-03-2026/nested 15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    char ch;
+    for(int i=n;i>=1;i--){
+        ch ='A';
+        for(int j=1;j<=i;j++){
+            printf("%c",ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
